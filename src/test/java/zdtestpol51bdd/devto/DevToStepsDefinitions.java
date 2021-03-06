3681155f55cfa9e306e89f0db95b52f03b380f91
lukaszkulawik <lukaszkulawik1@gmail.com>
2021-03-06 14:14:38 +0100

    completed test with firts blog

--- src/test/java/zdtestpol51bdd/devto/DevToStepsDefinitions.java
+++ src/test/java/zdtestpol51bdd/devto/DevToStepsDefinitions.java
@@ -4,17 +4,25 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DevToStepsDefinitions {
 
     WebDriver driver;
+    WebDriverWait wait;
+    String firstBlogTitle;
 
     @Before
     public void setup(){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
 
     }
 
@@ -24,10 +32,15 @@ public class DevToStepsDefinitions {
     }
     @When("I click on first blog displayed")
     public void i_click_on_first_blog_displayed() {
-        System.out.println("WHEN AND");
+        WebElement firstBlog = driver.findElement(By.cssSelector("h2.crayons-story__title > a") );
+        firstBlogTitle = firstBlog.getText();
+        firstBlog.click();
     }
     @Then("I should be redirected to blog page")
     public void i_should_be_redirected_to_blog_page() {
-        System.out.println("THEN");
+        wait.until(ExpectedConditions.titleContains(firstBlogTitle));
+        WebElement blogTitle = driver.findElement(By.tagName("h1"));
+        String blogTitleText = blogTitle.getText();
+        Assert.assertEquals(firstBlogTitle, blogTitleText);
     }
 }
