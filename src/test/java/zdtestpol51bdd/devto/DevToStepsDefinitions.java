4d1175511f3841e7e50288badf563c036efa1e8e
lukaszkulawik <lukaszkulawik1@gmail.com>
2021-03-06 11:41:07 +0100

    Move opening browser to before, changed when to given for devto opening

--- src/test/java/zdtestpol51bdd/devto/DevToStepsDefinitions.java
+++ src/test/java/zdtestpol51bdd/devto/DevToStepsDefinitions.java
@@ -14,14 +14,11 @@ public class DevToStepsDefinitions {
     @Before
     public void setup(){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-
-    }
-
-    @Given("Chrome browser is open")
-    public void chrome_browser_is_open() {
         driver = new ChromeDriver();
+
     }
-    @When("I go to devto main page")
+
+    @Given("I go to devto main page")
     public void i_go_to_devto_main_page() {
        driver.get("https://dev.to");
     }
--- src/test/resources/zdtestpol51bdd.devto/devTo.feature
+++ src/test/resources/zdtestpol51bdd.devto/devTo.feature
@@ -1,6 +1,5 @@
 Feature: devTo basic features
   Scenario: Open first seeing blog
-    Given Chrome browser is open
-    When I go to devto main page
-    And I click on first blog displayed
+    Given I go to devto main page
+    When I click on first blog displayed
     Then I should be redirected to blog page
