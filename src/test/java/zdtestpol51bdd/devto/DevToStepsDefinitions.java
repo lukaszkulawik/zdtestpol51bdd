4471e390a81eef429460313803f973e6aade4dc7
lukaszkulawik <lukaszkulawik1@gmail.com>
2021-03-07 15:59:56 +0100

    improved test with podcast

--- src/test/java/zdtestpol51bdd/devto/DevToStepsDefinitions.java
+++ src/test/java/zdtestpol51bdd/devto/DevToStepsDefinitions.java
@@ -17,28 +17,24 @@ import org.openqa.selenium.Keys;
 import java.util.List;
 
 public class DevToStepsDefinitions {
-
     WebDriver driver;
     WebDriverWait wait;
     String firstBlogTitle;
     String firstCastTitle;
     String searchingPhrase;
-
     @Before
     public void setup(){
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
-
+        wait = new WebDriverWait(driver,10);
     }
-
     @Given("I go to devto main page")
     public void i_go_to_devto_main_page() {
-       driver.get("https://dev.to");
+        driver.get("https://dev.to");
     }
     @When("I click on first blog displayed")
     public void i_click_on_first_blog_displayed() {
-        WebElement firstBlog = driver.findElement(By.cssSelector("h2.crayons-story__title > a") );
+        WebElement firstBlog = driver.findElement(By.cssSelector("h2.crayons-story__title > a"));
         firstBlogTitle = firstBlog.getText();
         firstBlog.click();
     }
@@ -49,58 +45,68 @@ public class DevToStepsDefinitions {
         String blogTitleText = blogTitle.getText();
         Assert.assertEquals(firstBlogTitle, blogTitleText);
     }
-
-    @When("I click text podcast in main page")
-    public void i_click_text_podcast_in_main_page() {
+    @When("I go to podcast section")
+    public void i_go_to_podcast_section() {
         WebElement podcast = driver.findElement(By.linkText("Podcasts"));
         podcast.click();
     }
-    @When("I click on first cast displayed")
-    public void i_click_on_first_cast_displayed() {
+    @When("I click on first podcast on the list")
+    public void i_click_on_first_podcast_on_the_list() {
         wait.until(ExpectedConditions.titleContains("Podcasts"));
         WebElement firstCast = driver.findElement(By.tagName("h3"));
         firstCastTitle = firstCast.getText();
         firstCastTitle = firstCastTitle.replace("podcast","");
         firstCast.click();
     }
-    @Then("I should be redirected to cast site")
-    public void i_should_be_redirected_to_cast_site(){
+    @Then("I should be redirected to podcast page")
+    public void i_should_be_redirected_to_podcast_page() {
         wait.until(ExpectedConditions.titleContains(firstCastTitle));
         WebElement castTitle = driver.findElement(By.tagName("h1"));
         String castTitleText = castTitle.getText();
         Assert.assertEquals(firstCastTitle, castTitleText);
     }
-
     @When("I search for {string} phrase")
     public void i_search_for_phrase(String phrase) {
         WebElement searchBar = driver.findElement(By.name("q"));
         searchBar.sendKeys(phrase);
         searchingPhrase = phrase;
         searchBar.sendKeys(Keys.ENTER);
-
     }
-
-    @Then("Top {int} blogs found should have correct phrase in title")
-    public void top_blogs_found_should_have_correct_phrase_in_title(Integer int1){
+    @Then("Top {int} blogs found should have correct phrase in title or snippet")
+    public void top_blogs_found_should_have_correct_phrase_in_title_or_snippet(Integer int1){
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3.crayons-story__title"))); //h3
         wait.until(ExpectedConditions.attributeContains(By.id("substories"),"class","search-results-loaded"));
         List<WebElement> allPosts = driver.findElements(By.className("crayons-story__body")); // div - caly wpis
-        if (allPosts.size() >= int1) {
-            for (int i = 0; i < int1; i++) {
+        if(allPosts.size() >= int1){
+            for (int i=0;i<int1;i++){
                 WebElement singlePost = allPosts.get(i);
                 WebElement singlePostTitle = singlePost.findElement(By.cssSelector(".crayons-story__title > a")); //tytul kafelka
                 String singlePostTitleText = singlePostTitle.getText().toLowerCase(); // wyciagnij tekst z tytulu
                 Boolean isPhraseInTitle = singlePostTitleText.contains(searchingPhrase);
-                if (isPhraseInTitle) { // isPhraseInTitle == true
+                if(isPhraseInTitle){ // isPhraseInTitle == true
                     Assert.assertTrue(isPhraseInTitle);
-                } else {
-                    WebElement snippet = singlePost.findElement(By.xpath("//div[contains(@class,'crayons-story__snippet')]"));
-                    String snippetText = snippet.getText().toLowerCase();
+                }
+                else{
+                    WebElement snippet = singlePost.findElement(By.xpath("//div[contains(@class,'crayons-story__snippet')]"));                 String snippetText = snippet.getText().toLowerCase();
                     Boolean isPhraseInSnippet = snippetText.contains(searchingPhrase);
                     Assert.assertTrue(isPhraseInSnippet);
                 }
             }
         }
+    }
+    @When("I play the podcast")
+    public void i_play_the_podcast() {
+        wait.until(ExpectedConditions.titleContains(firstCastTitle));
+        WebElement recordButton = driver.findElement(By.id("record"));
+        recordButton.click();
+    }
+    @Then("Podcast Should be played")
+    public void podcast_should_be_played() {
+        WebElement initializing = driver.findElement(By.className("status-message"));
+        wait.until(ExpectedConditions.invisibilityOf(initializing));
+        WebElement pauseBtn = driver.findElement(By.xpath("//img[contains(@class,'pause-butt')]"));
+        Boolean isPauseBtnVisible = pauseBtn.isDisplayed();
+        Assert.assertTrue(isPauseBtnVisible);
     }
 }
 
